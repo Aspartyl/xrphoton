b70ca424761e39576aa2597fd3dd0ce27faf077d
nrolfe1 <nrolfe@proton.me>
2026-06-14 20:34:08 -0400

    Create Vulkan instance with validation layer check

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,46 @@
+#include <cstring>
 #include <iostream>
+#include <vector>
 #include <vulkan/vulkan.h>
+
+namespace
+{
+constexpr const char* ValidationLayerName = "VK_LAYER_KHRONOS_validation";
+
+void printVulkanVersion(uint32_t version)
+{
+    std::cout << VK_VERSION_MAJOR(version) << '.'
+              << VK_VERSION_MINOR(version) << '.'
+              << VK_VERSION_PATCH(version);
+}
+
+bool isValidationLayerAvailable(const char* requestedLayerName)
+{
+    uint32_t layerCount = 0;
+    VkResult result = vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    if (result != VK_SUCCESS) {
+        std::cerr << "Failed to enumerate Vulkan instance layers.\n";
+        return false;
+    }
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    result = vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    if (result != VK_SUCCESS) {
+        std::cerr << "Failed to read Vulkan instance layer properties.\n";
+        return false;
+    }
+
+    for (const VkLayerProperties& layer : availableLayers) {
+        if (std::strcmp(layer.layerName, requestedLayerName) == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+}
 
 int main()
 {
@@ -13,10 +54,48 @@ int main()
         return 1;
     }
 
-    std::cout << "Vulkan instance version: "
-              << VK_VERSION_MAJOR(instanceVersion) << '.'
-              << VK_VERSION_MINOR(instanceVersion) << '.'
-              << VK_VERSION_PATCH(instanceVersion) << '\n';
+    std::cout << "Vulkan instance version: ";
+    printVulkanVersion(instanceVersion);
+    std::cout << '\n';
+
+    if (!isValidationLayerAvailable(ValidationLayerName)) {
+        std::cerr << "Required Vulkan validation layer is not available: "
+                  << ValidationLayerName << '\n';
+        return 1;
+    }
+
+    std::cout << "Using Vulkan validation layer: " << ValidationLayerName << '\n';
+
+    const char* enabledLayers[] = {
+        ValidationLayerName,
+    };
+
+    VkApplicationInfo applicationInfo{};
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pApplicationName = "xrPhoton";
+    applicationInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    applicationInfo.pEngineName = "xrPhoton";
+    applicationInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+    applicationInfo.apiVersion = instanceVersion;
+
+    VkInstanceCreateInfo instanceCreateInfo{};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+    instanceCreateInfo.enabledLayerCount = 1;
+    instanceCreateInfo.ppEnabledLayerNames = enabledLayers;
+
+    VkInstance instance = VK_NULL_HANDLE;
+    const VkResult createResult = vkCreateInstance(&instanceCreateInfo, nullptr, &instance);
+
+    if (createResult != VK_SUCCESS) {
+        std::cerr << "Failed to create Vulkan instance.\n";
+        return 1;
+    }
+
+    std::cout << "Created Vulkan instance.\n";
+
+    vkDestroyInstance(instance, nullptr);
+    std::cout << "Destroyed Vulkan instance.\n";
 
     return 0;
 }
